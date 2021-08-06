06959f4b7d1de9eee2b33459ea2ac0e2b774fb9f
Rathapong <dev01tcm@outlook.com>
2021-08-06 13:49:18 +0700

    บทความ

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import("./bootstrap.min.css");
 
 const Header = lazy(() => import("./Header"));
 const Cardgame = lazy(() => import("./Cardgame"));
+const Article = lazy(() => import("./Article"));
 render(
   <StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
       <Header />
       <Cardgame />
+      <Article/>
     </Suspense>
   </StrictMode>,
   document.getElementById("root")
