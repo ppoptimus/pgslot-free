9d5fe890264e9f06254b0c96ea2cc596d91fc8ae
ppoptimus <r.pumpo@gmail.com>
2021-08-06 07:52:32 +0700

    first

---
+++ src/Cardgame.js
@@ -0,0 +1,243 @@
+import React from "react";
+
+export default function Cardgame() {
+  return (
+    <div>
+      <div className="container-fluid bg-content">
+        <h2 className="mt-2">ยินดีต้อนรับเข้าสู่อณาจักรเกม SLOT</h2>
+        <h3>ทดลองเล่นฟรีแบบไม่อั้น กับเกมส์หลากหลาย Style แบบจุใจได้ที่นี่</h3>
+        <p>
+          <strong>เกมสล็อต PGSLOT</strong>&nbsp;ที่เราได้คัดสรร และจัดเตรียมไว้ให้บริการสมาชิกทุกท่าน มีหลากหลายรูปแบบ หลากหลายเกม ด้วยภาพที่ละเอียด เสียงเอฟเฟกต์สมจริง สร้างความตื่นเต้น เร้าใจ
+          ให้กับผู้เล่นได้ในทุกฟีเจอร์ เปิดประสบการณ์ใหม่ ด้วยการเล่นเกมสล็อตออนไลน์ ของ&nbsp;<strong>PGSLOT</strong>&nbsp;เกมสล็อตออนไลน์ รูปแบบใหม่ ที่มาแรงที่สุดในขณะนี้&nbsp;รับรองว่าทุกท่าน
+          จะได้รับความตื่นตา ตื่นใจ กับเกมสล็อตที่เรานำเสนอแน่นอน อย่าพลาดโอกาสรับเงินรางวัลใหญ่ จากเกมสล็อตออนไลน์ของเรา สมัครสมาชิกใหม่ เพื่อเล่นเกมแบบได้เงินจริงชัวร์ๆ คลิก&nbsp;
+          <b>
+            <a href="https://watafak88.com/#/playgame"><button>Play Game</button></a>
+          </b>
+        </p>
+        <div className="row text-center justify-content-center">
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/rise-of-apollo/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer" >
+              <img src="img/pgslot1.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Rise of Apollo</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/bali-vacation/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot2.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Bali Vacation</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/wild-bandito/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot3.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Wild Bandito</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/candy-burst/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot4.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Candy Bonanza</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/circus-delight/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot5.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Circus Delight</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/crypto-gold/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot6.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Crypto Gold</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/dreams-of-macau/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot7.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Dreams of Macau</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/fortune-ox/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot8.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Fortune Ox</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/galactic-gems/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US,en;q=0.9&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https://dev.pgslot.co/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot9.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Galactic Gems</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/gdn-ice-fire/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot10.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Guardians of Ice & Fire</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/heist-stakes/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot11.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Heist Stakes</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/jack-frosts/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot12.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Jack Frost's Winter</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/jewels-prosper/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot13.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Jewels of Prosperity</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/bikini-paradise/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot14.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Bikini Paradise</strong>
+            </div>
+          </div>
+        
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/lucky-neko/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot15.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Lucky Neko</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/mermaid-riches/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US&bet_type=2&operator_token=8735ze6y8kp7jpwmxvau7gvytu3adwj4&from=https%3A%2F%2Fpublic.pg-redirect.com%2Fpages%2Fclose.html" target="_blank" rel="noreferrer">
+              <img src="img/pgslot16.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Mermaid Riches</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/opera-dynasty/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot17.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Opera Dynasty</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/queen-bounty/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot18.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Queen of Bounty</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/sct-cleopatra/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot19.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Secrets of Cleopatra</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/sushi-oishi/index.html?__refer=m.pg-redirect.com&__sv=0&language=en-US&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https%3A%2F%2Fpublic.pg-redirect.com%2Fpages%2Fclose.html" target="_blank" rel="noreferrer">
+              <img src="img/pgslot20.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Sushi Oishi</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/thai-river/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot21.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Thai River Wonders</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/majestic-ts/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot22.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Majestic Treasures</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/dragon-hatch/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot23.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Dragon Hatch</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/treasures-aztec/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot24.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Treasures of Aztec</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/vampires-charm/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot25.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Vampire's Charm</strong>
+            </div>
+          </div>
+          <div className="card m-1" style={{ width: "180px", padding: 0 }}>
+            <a href="https://m.pg-demo.com/ways-of-qilin/index.html?__refer=m.pg-redirect.com&__sv=0&language=th-TH,th;q=0.9&bet_type=2&operator_token=fu6fpagpekf7445m6sdeecr8xvkkfvy6&from=https://dev.pgslot.cc/home" target="_blank" rel="noreferrer">
+              <img src="img/pgslot26.jpg" className="img-fluid" width="180px" height="241px" alt="pg slot" />
+            </a>
+            <div className="card-body">
+              <strong>Ways of the Qilin</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="line-button">
+        <a href="https://lin.ee/bDb5ic7" target="blank" rel="noopener">
+          <img rel="preload" as="image" imagesizes="50vw" src="img/line_icon.svg" className="img-fluid" alt="@watafak88"></img>
+        </a>
+      </div>
+
+      <div className="facebook-button">
+        <a href="https://www.facebook.com/Watafak88/" target="blank" rel="noopener">
+          <img rel="preload" as="image" imagesizes="50vw" src="img/facebook_icon.svg" className="img-fluid" alt="watafak88 fanpage"></img>
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode, Suspense, lazy } from "react";
+import { render } from "react-dom";
+import("./bootstrap.min.css");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = lazy(() => import("./Header"));
+const Cardgame = lazy(() => import("./Cardgame"));
+render(
+  <StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+      <Cardgame />
+    </Suspense>
+  </StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
